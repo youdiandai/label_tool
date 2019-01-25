26a141b733fdbd49597d4ca45ee1aceb79adf539
ChangXin <1561753947@qq.com>
2019-01-25 13:46:42 +0800

    更改了图像分类中分类信息的刷新方式，减少了对/project/mark_count的请求

--- app/static/js/image_navigation.js
+++ app/static/js/image_navigation.js
@@ -27,13 +27,13 @@ function createImgNavi() {
 function divCreateImg(div) {
     //在imageDiv标签内部创建img图片，使用div的属性，并设置图片点击后跳转
     div.style.display = null;
-    var img = document.createElement('img');
+    let img = document.createElement('img');
     img.src = div.getAttribute('img_src');
     img.className = 'little_img';
     img.onclick = function () {
         refreshImgNavi(parseInt(img.parentElement.id.split('_')[2]));
     };
-    var _status_div = null;
+    let _status_div = null;
     if (div.childElementCount > 0) {
         _status_div = div.firstChild;
         div.innerHTML = ""
@@ -42,12 +42,12 @@ function divCreateImg(div) {
     if (_status_div) {
         div.appendChild(_status_div);
     }
-};
+}
 
 function get_img_status(marked_and_types) {
-    var status = [];
-    for (var y = 0; y < marked_and_types.length; y++) {
-        var t = Object();
+    let status = [];
+    for (let y = 0; y < marked_and_types.length; y++) {
+        let t = Object();
         t.marked = marked_and_types[y][0];
         t.type = marked_and_types[y][1];
         status.push(t)
@@ -58,8 +58,8 @@ function get_img_status(marked_and_types) {
 function addImgStatus(div, index, status) {
     //给div添加图片状态
     if (status[index].marked) {
-        var flag = false;
-        for (var i = 0; i < div.children.length; i++) {
+        let flag = false;
+        for (let i = 0; i < div.children.length; i++) {
             if (div.children[i].tagName === 'DIV') {
                 flag = true;
                 div.children[i].setAttribute('data-text', status[index].type)
@@ -69,7 +69,7 @@ function addImgStatus(div, index, status) {
 
         }
         else {
-            var new_div = document.createElement('div');
+            let new_div = document.createElement('div');
             new_div.className = 'tiny_div';
             new_div.setAttribute('data-text', status[index].type);
             new_div.onclick = function () {
@@ -81,8 +81,8 @@ function addImgStatus(div, index, status) {
 }
 
 function refreshImgStatus(status) {
-    var images = document.getElementById('little_images').children;
-    for (var i = 0; i < images.length; i++) {
+    let images = document.getElementById('little_images').children;
+    for (let i = 0; i < images.length; i++) {
         addImgStatus(images[i], i, status)
     }
 }
@@ -91,7 +91,7 @@ function refreshImgStatus(status) {
 function divCleanImg(div) {
     //清除div中的图片
     div.style.display = 'none';
-    var _tmp = null;
+    let _tmp = null;
     if (div.childElementCount === 2) {
         _tmp = div.children[1];
     }
@@ -112,20 +112,20 @@ function divSetCurrent(div) {
 
 function imgNaviNext() {
     //跳转到下一张图片，注意，没有跳转canvans中的图片，所以canvans中的图片还需要另外进行刷新，并修改current_index的值，目前在点击下一张时调用
-    var index = current_index + 1;
-    var images = document.getElementById('little_images').children;
+    let index = current_index + 1;
+    let images = document.getElementById('little_images').children;
     if (index < images.length) {
-        for (var i = 0; i < images.length; i++) {
+        for (let i = 0; i < images.length; i++) {
             divCleanImg(images[i]);
         }
-        var display_img = [];
+        let display_img = [];
         if (index < 2) {
-            for (var i = 0; i < 5; i++) {
+            for (let i = 0; i < 5; i++) {
                 display_img.push(images[i]);
             }
         }
         else if (index > images.length - 3) {
-            for (var i = images.length - 1; i > images.length - 6; i--) {
+            for (let i = images.length - 1; i > images.length - 6; i--) {
                 display_img.push(images[i]);
             }
         }
@@ -145,20 +145,20 @@ function imgNaviNext() {
 
 function imgNaviPrev() {
     //跳转到上一张图片，同imgNaviNext。
-    var index = current_index - 1;
+    let index = current_index - 1;
     if (index >= 0) {
-        var images = document.getElementById('little_images').children;
-        for (var i = 0; i < images.length; i++) {
+        let images = document.getElementById('little_images').children;
+        for (let i = 0; i < images.length; i++) {
             divCleanImg(images[i]);
         }
         var display_img = [];
         if (index < 2) {
-            for (var i = 0; i < 5; i++) {
+            for (let i = 0; i < 5; i++) {
                 display_img.push(images[i]);
             }
         }
         else if (index > images.length - 3) {
-            for (var i = images.length - 1; i > images.length - 6; i--) {
+            for (let i = images.length - 1; i > images.length - 6; i--) {
                 display_img.push(images[i]);
             }
         }
@@ -169,7 +169,7 @@ function imgNaviPrev() {
             display_img.push(images[index + 1]);
             display_img.push(images[index + 2]);
         }
-        for (var i = 0; i < display_img.length; i++) {
+        for (let i = 0; i < display_img.length; i++) {
             divCreateImg(display_img[i]);
         }
         divSetCurrent(images[index]);
@@ -178,14 +178,14 @@ function imgNaviPrev() {
 
 function viewNext() {
     //导航条向右挪动一格
-    var images = document.getElementById('little_images').children;
-    var showing_img = [];
-    for (var i = 0; i < images.length; i++) {
+    let images = document.getElementById('little_images').children;
+    let showing_img = [];
+    for (let i = 0; i < images.length; i++) {
         if (images[i].childElementCount > 1 || (images[i].childElementCount > 0 && images[i].firstChild.tagName === 'IMG')) {
             showing_img.push(images[i])
         }
     }
-    var _next = showing_img[showing_img.length - 1].nextElementSibling;
+    let _next = showing_img[showing_img.length - 1].nextElementSibling;
     if (_next) {
         divCreateImg(_next);
         if (parseInt(_next.id.split('_')[2]) === current_index) {
@@ -197,14 +197,14 @@ function viewNext() {
 
 function viewPrev() {
     //导航条向左挪动一格
-    var images = document.getElementById('little_images').children;
-    var showing_img = [];
-    for (var i = 0; i < images.length; i++) {
+    let images = document.getElementById('little_images').children;
+    let showing_img = [];
+    for (let i = 0; i < images.length; i++) {
         if (images[i].childElementCount > 1 || (images[i].childElementCount > 0 && images[i].firstChild.tagName === 'IMG')) {
             showing_img.push(images[i])
         }
     }
-    var _prev = showing_img[0].previousSibling;
+    let _prev = showing_img[0].previousSibling;
     if (_prev) {
         divCreateImg(_prev);
         if (parseInt(_prev.id.split('_')[2]) === current_index) {
@@ -217,13 +217,13 @@ function viewPrev() {
 
 function refreshImgNavi(index) {
     //刷新导航条为以index为current_index的情况
-    var images = document.getElementById('little_images').children;
-    for (var i = 0; i < images.length; i++) {
+    let images = document.getElementById('little_images').children;
+    for (let i = 0; i < images.length; i++) {
         divCleanImg(images[i]);
     }
-    var display_img = [];
+    let display_img = [];
     if (index < 2) {
-        for (var i = 0; i < 5; i++) {
+        for (let i = 0; i < 5; i++) {
             display_img.push(images[i]);
         }
     }
@@ -239,7 +239,7 @@ function refreshImgNavi(index) {
         display_img.push(images[index + 1]);
         display_img.push(images[index + 2]);
     }
-    for (var i = 0; i < display_img.length; i++) {
+    for (let i = 0; i < display_img.length; i++) {
         divCreateImg(display_img[i]);
     }
     divSetCurrent(images[index]);
